dea5d473fdb6800111815fdec85abebd2e709ae8
Paulo Murilo Matielo de Oliveira <116845901+Paulo-Murilo-Dev@users.noreply.github.com>
2024-10-23 21:38:14 -0300

    adicionando as parada
    paradas adicionadas

---
+++ nao_clique.bat
@@ -0,0 +1,44 @@
+@echo off
+title Sistema Criptografado
+color 4F
+mode 1000
+
+echo **      **     **     **      **     **     **      **     **    
+echo /**     /**    ****   /**     /**    ****   /**     /**    ****   
+echo /**     /**   **//**  /**     /**   **//**  /**     /**   **//**  
+echo /**********  **  //** /**********  **  //** /**********  **  //** 
+echo /**//////** **********/**//////** **********/**//////** **********
+echo /**     /**/**//////**/**     /**/**//////**/**     /**/**//////**
+echo /**     /**/**     /**/**     /**/**     /**/**     /**/**     /**
+echo //      // //      // //      // //      // //      // //      //
+
+echo SE-FODA COMUNISTA SAFADO
+timeout /t 2 /nobreak >nul
+
+echo Inicializando criptografia...
+cls
+
+echo Criptografando seus arquivos... [#                       ] 4%%
+timeout /t 1 /nobreak >nul
+cls
+echo Criptografando seus arquivos... [######################  ] 84%%
+timeout /t 1 /nobreak >nul
+cls
+echo Criptografando seus arquivos... [####################### ] 100%%
+timeout /t 1 >nul
+cls
+
+echo TODOS OS SEUS ARQUIVOS FORAM CRIPTOGRAFADOS.
+echo.
+echo Se quiser se safar, envie 0,26 bitcoin para:
+echo 1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa
+
+REM Olá meu querido amigo, não se preocupe com isso, bem, te conhecendo eu sei que você nunca iria cair nesse truque idiota, afinal você é tão inteligente quanto eu.
+REM Já para você, meu querido agente do partido comandado por demônios aqui vai uma pequena charada:
+REM 096ca47811d39d3f54495adc93b11a4b6f4140332d7a0ce0b177ba00ae6327db
+
+REM Boa sorte, sei que vocês são um bando de acéfalos sem honra, faça um favor ao mundo e cumpra o desejo da mensagem. 
+
+
+
+pause > nul
